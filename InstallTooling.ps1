c8c4edaf320e8d71ba4f46dabe03c6f3603967ce
Jens Torgeir Solaugsten <jens-torgeir.solaugsten@sarpsborg.com>
2020-11-24 14:49:56 +0100

    Changes

--- InstallTooling.ps1
+++ InstallTooling.ps1
@@ -1,22 +1,34 @@
-# Install
+#Requires -RunAsAdministrator
 
 # Install visual studio extensions
-code --install-extension ms-python.python
-code --install-extension ms-dotnettools.csharp
-code --install-extension ms-vscode.powershell
-code --install-extension ms-vscode.azure-account
-code --install-extension ms-vscode.azurecli
-code --install-extension vscode-icons-team.vscode-icons
-code --install-extension VisualStudioExptTeam.vscodeintellicode
-code --install-extension nobuhito.printcode
-code --install-extension esbenp.prettier-vscode
-code --install-extension ms-mssql.mssql
-code --install-extension ms-vscode-remote.remote-ssh-edit
-code --install-extension ms-edgedevtools.vscode-edge-devtools
-code --install-extension ms-vscode.hexeditor
-code --install-extension ms-vscode.vscode-node-azure-pack
-code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-code --install-extension vsciot-vscode.azure-iot-tools
+$vcmodules = @('ms-python.python',
+    'ms-dotnettools.csharp',
+    'ms-vscode.powershell',
+    'ms-vscode.azure-account',
+    'ms-vscode.azurecli',
+    'vscode-icons-team.vscode-icons',
+    'VisualStudioExptTeam.vscodeintellicode'
+    'nobuhito.printcode',
+    'esbenp.prettier-vscode',
+    'ms-mssql.mssql',
+    'ms-vscode-remote.remote-ssh-edit',
+    'ms-edgedevtools.vscode-edge-devtools'
+    'ms-vscode.hexeditor',
+    'ms-vscode.vscode-node-azure-pack'
+    'ms-vscode-remote.vscode-remote-extensionpack',
+    'vsciot-vscode.azure-iot-tools'
+    'zhuangtongfa.material-theme')
+
+Write-Host -ForegroundColor Yellow "Installing Visual Studio Code Extensions:"
+$instvcmodules = (code --list-extensions)
+foreach ($vcmodule in $vcmodules) {
+    if ($instvcmodules -match $vcmodule) {
+        Write-Host -ForegroundColor White $vcmodule" preinstalled. Skipping..."
+    }
+    else {
+        code --install-extension $vcmodule
+    }
+}
 
 # Install Visual Studio Code settings
 copy-item -path .\vscsettings.json $env:USERPROFILE\AppData\Roaming\Code\user\settings.json
